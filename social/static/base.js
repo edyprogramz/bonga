84c674ad4763123cfa3443df243549ec296d9c3b
edyprogramz <116636391+edyprogramz@users.noreply.github.com>
2023-06-07 13:55:31 +0300

    update

---
+++ social/static/base.js
@@ -0,0 +1,10 @@
+const navImgClickDiv = document.querySelector('.navbar-img-click-options');
+const divClicking = document.querySelector('.nav-img-click');
+
+divClicking.onclick = () => {
+    if (navImgClickDiv.style.display === "block"){
+        navImgClickDiv.style.display = "none";
+    } else {
+        navImgClickDiv.style.display = "block"; 
+    }
+}
