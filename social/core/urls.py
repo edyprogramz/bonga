21ad131dbd884495ca89a6251ae5d5aa19965850
edyprogramz <116636391+edyprogramz@users.noreply.github.com>
2023-06-08 10:45:22 +0300

    login page Signup

--- social/core/urls.py
+++ social/core/urls.py
@@ -6,4 +6,6 @@ app_name = "core"
 urlpatterns = [
     path('', views.index, name="home"),
     path('signup/', views.signup, name="signup"),
+    path('login/', views.login, name="login"),
+
 ]
--- social/core/views.py
+++ social/core/views.py
@@ -6,3 +6,6 @@ def index(request):
 
 def signup(request):
     return render(request, "auth/signup.html", {})
+
+def login(request):
+    return render(request, "auth/login.html", {})
